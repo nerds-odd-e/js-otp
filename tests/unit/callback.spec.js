003edd5f455001391a6d935ee2bbde7d4f0d5d3e
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-09-15 15:17:30 +0800

    :sparkles: add second verification as a mock verify sample

--- tests/unit/callback.spec.js
+++ tests/unit/callback.spec.js
@@ -3,13 +3,14 @@ import Callback from '@/views/Callback'
 
 describe('callback', function () {
 
-    it('should set something', async function () {
+    it('should set something after 2 seconds', async function () {
         const component = shallowMount(Callback, {localVue: createLocalVue()});
         window.setTimeout = jest.fn(callback => callback())
 
         component.find('button.go').trigger('click')
 
         expect(component.vm.$data.something).toEqual('Hello World')
+        expect(window.setTimeout).toBeCalledWith(expect.anything(), 2000)
     });
 
 });
