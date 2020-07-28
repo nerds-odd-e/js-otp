e97df877ed98b35c4a753d5e0ad6b4a11f2c67c2
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2020-07-28 18:05:28 +0800

    :sparkles: add ut for store entry when mount

--- tests/unit/about.spec.js
+++ tests/unit/about.spec.js
@@ -14,15 +14,36 @@ const givenRandomApi = entry => {
     })
 }
 
+var wrapper
+
+const createStoreWithApiName = () => {
+    const store = createStore()
+    store.state.entity.entries[0].API = 'storeApiName'
+    return store
+}
+
 describe('About', () => {
-    test('should show a random api name', async() => {
+    beforeEach(() => {
+        givenRandomApi({
+            API: 'apiName'
+        })
+    })
+
+    test('should show a random api name by api call', async() => {
         givenRandomApi({
             API: 'apiName'
         })
 
-        var wrapper = await mount(About, { store: createStore(), localVue: createLocalVue() })
+        wrapper = await mount(About, { store: createStore(), localVue: createLocalVue() })
         await flushPromises()
 
         expectAllTextExists(wrapper, 'apiName')
+    })
+
+    test('should show an api name by store', async() => {
+        wrapper = await mount(About, {store: createStoreWithApiName(), localVue: createLocalVue()})
+        await flushPromises()
+
+        expectAllTextExists(wrapper, 'storeApiName')
     })
 })
